dd79ece4683d60b05b58e568f487345b125e2e08
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-09-20 23:58:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let doc = document.getElementById("form")
+	let firstname = document.getElementById("input")[0].value
+	let lastname = document.getElementById("input")[1].value
+	
 
 }
